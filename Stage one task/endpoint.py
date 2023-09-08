41bba577b286786d9547e4e7dbcd95c890dfc965
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-09-08 13:28:36 +0100

    endpoint

--- Stage one task/endpoint.py
+++ Stage one task/endpoint.py
@@ -2,16 +2,20 @@ from flask import Flask, request, jsonify
 
 endpoint = Flask(__name__)
 
-@end_point.route('/endpoint', methods=['GET'])
+@endpoint.route('/endpoint', methods=['GET'])
 def myAPI():
+    git_file_url = request.args.get('https://github.com/cj-flute/repo/blob/main/endpoint.py')
+    git_repo_url = request.args.get('https://github.com/cj-flute/repo')
     infoAPI = {
         "slack_name": "CjFlute",
         "current_day": "Friday",
         "utc_time": "2023-09-08T12:09:05Z",
         "track": "backend",
-        "github_file_url": "https://github.com/cj-flute/repo/blob/main"
+        "github_file_url": git_file_url,
+        "github_repo_url": git_repo_url,
+        "status_code": 200,
     }
-    return api
+    return jsonify(infoAPI)
     pass
 
 if __name__ == "__main__":
