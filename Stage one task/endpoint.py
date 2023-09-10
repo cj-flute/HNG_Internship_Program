fece749caf11d7e9598b0b87054297bc20118ea2
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-09-10 23:45:04 +0100

    endpoint

--- Stage one task/endpoint.py
+++ Stage one task/endpoint.py
@@ -10,17 +10,27 @@ endpoint = Flask(__name__)
 def myAPI():
     q = request.args.to_dict()
 
+    # date_time_year = datetime.datetime.now()
+    # date = date_time_year.isoformat()
+    # day = datetime.date(int(date_time_year.year),
+    #     int(date_time_year.month), int(date_time_year.day)).strftime("%A")
+
+        # For the date and time (auto-generated)
+    dt = datetime.date.today()
+    t = time.strftime("%H:%M:%S")
+    dtt = f"{dt}T{t}Z"
     date_time_year = datetime.datetime.now()
-    date = date_time_year.isoformat()
+    ttt = date_time_year.isoformat()
     day = datetime.date(int(date_time_year.year),
         int(date_time_year.month), int(date_time_year.day)).strftime("%A")
+
 
  
     # My API
     infoAPI = {
         "slack_name": q.get("slack_name"),
         "current_day": day,
-        "utc_time": date,
+        "utc_time": dtt,
         "track": q.get("track"),
         "github_file_url": 'https://github.com/cj-flute/repo/blob/main/endpoint.py',
         "github_repo_url": 'https://github.com/cj-flute/repo',
